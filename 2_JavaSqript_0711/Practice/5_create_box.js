fc74e32503c8c6f90c895db54ed767bfea1b3414
葛葉 朋彦 <tomohiko-kuzuba@MC9A-0011.local>
2022-07-13 20:14:33 +0900

    files modify

---
+++ 2_JavaSqript_0711/Practice/5_create_box.js
@@ -0,0 +1,16 @@
+// 1. ボタンを押されるまで待機
+// 2.
+
+function add_box(){
+
+}
+
+function del_add(){
+
+}
+
+let add = document.querySelector("#add")
+let del = document.querySelector("#del")
+
+blue.addEventListener("click", add_box); 
+blue.addEventListener("click", del_add); 
