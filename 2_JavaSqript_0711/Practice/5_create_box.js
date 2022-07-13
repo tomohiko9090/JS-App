05e613c37eadbddaea3e52f5bcf18392070fac13
hikotomo <tomo.k9090@gmail.com>
2022-07-14 08:14:28 +0900

    files modify

--- 2_JavaSqript_0711/Practice/5_create_box.js
+++ 2_JavaSqript_0711/Practice/5_create_box.js
@@ -2,15 +2,25 @@
 // 2. ボタン押されたら四角が表示
 
 function add_box(){
-
+  box_count += 1;
+  let box_all = "";  
+  for (i=0; i<=box_count; i++){
+    box_all = box_all + box_text
+  }
+  document.write(box_text)
+  box.outerHTML = box_all;
 }
 
 function del_add(){
-
+  box_count -= 1
 }
+let box_count = 0;
+box_text = "<span></span>"
 
 let add = document.querySelector("#add")
 let del = document.querySelector("#del")
+// let box_drow = document.querySelector("#box")
 
-blue.addEventListener("click", add_box); 
-blue.addEventListener("click", del_add); 
+add.addEventListener("click", add_box); 
+del.addEventListener("click", del_add); 
+
