f46003b185785231b6f6beba84726aeadebe14cd
hikotomo <tomo.k9090@gmail.com>
2022-07-16 17:16:26 +0900

    files modify

---
+++ 2_JavaSqript_0711/Practice/8_test.js
@@ -0,0 +1,14 @@
+let dice_dict = {1: "dice_1.png", 2: "dice_2.png", 3: "dice_3.png", 4: "dice_4.png", 5: "dice_5.png", 6: "dice_6.png"}
+let dice_list = [1, 2, 3, 4, 5, 6]
+let num = dice_list[Math.floor(Math.random() * dice_list.length)];
+
+
+let a = 2
+let b = 2
+let c = 2
+
+if (a == b){
+  console.log("いけてる")
+}
+
+
