d955a16ec1a3ac113bf3fd8ac77bc9f6e6e0381c
hikotomo <tomo.k9090@gmail.com>
2022-07-23 22:22:33 +0900

    files modify

--- 4_Ruby/Practice/2_attack_or_defense.rb
+++ 4_Ruby/Practice/2_attack_or_defense.rb
@@ -1,50 +1,68 @@
-# 勝ち負け判定
+# 勝ち負け判定メソッド
 def judge(you, enemy, number_of_winnig)
-  p "a [Attack] OR d [Defense] : "
-  p command = gets.to_s
-  sleep(2)
+  start_time = Time.now
+  print "a [Attack] OR d [Defense] : "
+  command = gets.chomp
+  
+  # コマンドミス→即負け
+  if command != "a" && command != "d"
+    puts "Command error"
+    return number_of_winnig
+  end
+
+  # 2秒以上経過→即負け
+  if 2 <= (Time.now - start_time) 
+    puts "Time over"
+    return number_of_winnig
+  end
 
   case command
   when "a"
     if you < enemy 
       puts "You lose"
+      return number_of_winnig
     end
     if enemy < you
       puts "You win"
       number_of_winnig += 1
+      return number_of_winnig
     end
 
   when "d"
     if you < enemy
       puts "You win"
       number_of_winnig += 1
+      return number_of_winnig
     end
     if enemy < you 
       puts "You lose"
+      return number_of_winnig
     end
   end
 end
-
-
 
 number_of_winnig = 0
 
-5.times do
+5.times do |i|
+  you = 0
+  enemy = 0 
 
-  # 自分と敵の値を決める
+  # ①自分と敵の値を決める
   loop do
     you = rand(9)
     enemy = rand(9)
-    if you == enemy
+    if you != enemy
       break
     end
   end
-
-  # 伝える
+  
+  # ②数字を報告
   puts "You: #{you} Enemy: #{enemy}"
 
-  # you,enemy → 
-  judge(you, enemy, number_of_winnig)
+  # ③対決
+  number_of_winnig = judge(you, enemy, number_of_winnig)
 
 end
-puts ""
+
+# ④結果発表
+puts "#{number_of_winnig}勝#{5-number_of_winnig}敗"
