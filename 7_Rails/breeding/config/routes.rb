1bd070586aaabee6d4c52840b0103ad250c14715
hikotomo <tomo.k9090@gmail.com>
2022-08-10 07:41:32 +0900

    files modify

--- 7_Rails/breeding/config/routes.rb
+++ 7_Rails/breeding/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-  get 'pets/new'
-  get 'owners/index'
-  get 'owners/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'owners/index'
+  # get 'owners/new'
+  resources :owners
+  resources :pets
+
 end
