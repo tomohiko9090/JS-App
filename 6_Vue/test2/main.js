34ee1070b21f06fe04c916177af7e6856b9802a1
hikotomo <tomo.k9090@gmail.com>
2022-09-17 12:41:05 +0900

    files modify

--- 6_Vue/test2/main.js
+++ 6_Vue/test2/main.js
@@ -1,36 +1,11 @@
-// 最初にインスタンスを生成している
 const app = Vue.createApp({
-    // データオブジェクト（コンポーネントの初期値設定）
     data(){
         return {
-            message: "Hello World!",
-            base: 0,
-            height:0,
-            radius:0,
-
-            products:[
-                {name: "りんご", price: 100, stock: 15},
-                {name: "みかん", price: 78, stock: 10},
-                {name: "バナナ", price: 58, stock: 30},
-                {name: "パイナップル", price: 380, stock: 6},
-            ],
-
-            numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-
-            activeTab: 'tabs-1', 
-            tabs: [
-                {name:'tabs-1', text: 'タブ1'},
-                {name:'tabs-2', text: 'タブ2'},
-                {name:'tabs-3', text: 'タブ3'},
-            ],
-            
-            hands: ["グー", "チョキ", "パー"],
-            you: null,
-            com: null,
+            newBook: {title: "", author: ""},
+            books: [],
         }
     },
 
-    // 算出プロパティ
     computed: {
         area(){
             return this.base * this.height / 2
@@ -52,7 +27,7 @@ const app = Vue.createApp({
     }
 })
 
-const vm = app.mount("#app") //インスタンスをマウントする（OSに認識させ、使えるようにする）蹴り飛ばしてるイメージ
+const vm = app.mount("#app") 
 
 
 
